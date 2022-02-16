cc99dae34b89b25dea9fe06da5f9de56c670e853
hinal.patel <hinal.patel@1R-79>
2022-02-16 16:43:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCFDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                char[] characters = { 'H', 'I', 'N' };
+                Console.WriteLine(characters[0]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("First");
+                Console.WriteLine("Second:" + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.WriteLine("@@@@@@@@@@@@");
+                Console.WriteLine("Fire");
+                Console.WriteLine("@@@@@@@@@@@@");
+            }
+            
+            
+
+            Console.ReadKey();
+        }
+    }
+}
